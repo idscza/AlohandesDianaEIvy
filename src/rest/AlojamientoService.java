2a0b2a13ee477d8f91dfbbb12d167df493fda263
Diana <dv.gonzalez10@uniandes.edu.co>
2018-04-18 19:31:53 -0500

    service incompleto

--- src/rest/AlojamientoService.java
+++ src/rest/AlojamientoService.java
@@ -1,5 +1,39 @@
 package rest;
 
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
 public class AlojamientoService {
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// ATRIBUTOS
+	//----------------------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Atributo que usa la anotacion @Context para tener el ServletContext de la conexion actual.
+	 */
+	@Context
+	private ServletContext context;
+
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS DE INICIALIZACION
+	//----------------------------------------------------------------------------------------------------------------------------------
+	
+	/**
+	 * Metodo que retorna el path de la carpeta WEB-INF/ConnectionData en el deploy actual dentro del servidor.
+	 * @return path de la carpeta WEB-INF/ConnectionData en el deploy actual.
+	 */
+	private String getPath() {
+		return context.getRealPath("WEB-INF/ConnectionData");
+	}
+
+	private String doErrorMessage(Exception e){
+		return "{ \"ERROR\": \""+ e.getMessage() + "\"}" ;
+	}
+	
+	//----------------------------------------------------------------------------------------------------------------------------------
+	// METODOS REST
+	//----------------------------------------------------------------------------------------------------------------------------------
+
 
 }
